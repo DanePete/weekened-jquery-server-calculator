4c7081d5633b6427357a069dc30882cab1155c35
Dane Petersen <dane.m.petersen@gmail.com>
2021-07-29 23:50:01 -0500

    working on equation calcuations and shipping data to and from server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,66 @@
 console.log($);
+
+$(document).ready(onReady);
+
+let inputString = '';
+let inputArray = [];
+
+function onReady() {
+  console.log('ready');
+  // $(document).on('click','#submitBtn', bundle);
+  $(document).on('click','#clear', clearInput);
+  $(document).on('click', '#key', createInput);
+  $(document).on('click', '#equals', bundle);
+  getResults();
+}
+
+function createInput() {
+  console.log($(this).data('key'));
+  inputString += $(this).data('key');
+  inputArray.push($(this).data('key'));
+  console.log('input array', inputArray);
+  console.log('input string',inputString);
+  $("#input1").val(inputString);
+  console.log('eval', eval(inputString));
+  // $(this).data(key);
+}
+
+function bundle() {
+  let val = $("#input1").val();
+  console.log(val);
+  $.ajax({
+    method: 'POST',
+    url: '/bundle',
+    data: {val: val}
+  }).then((response) => {
+    console.log('POST /bundle', response);
+  }).catch((error) => {
+    console.log('failed', error);
+    $('body').prepend('<h2>ERROR</h2>');
+  });
+  $('#input1').val('');
+  getResults();
+}
+
+function getResults() {
+  $.ajax({
+    type: 'GET',
+    url: '/result'
+  }).then(function (response) {
+    console.log('my rsponse asfresponse', response);
+    for (let i = 0; i < response.length; i++) {
+        let equation = response[i];
+        $('#results').append(`
+            <tr>
+                <td>${equation}</td>
+            </tr>
+        `);
+    }
+  });
+}
+
+function clearInput() {
+ $('#input1').val('');
+
+}
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { response } = require('express');
 const app = express();
 const PORT = 5000;
+let bundleCalculations = [];
 
 // Get Response from client
 
@@ -12,6 +13,25 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
+
+app.post('/bundle', function(request, response) {
+  console.log('Ready to take in an equation');
+  console.log('request.route.path is', request.route.path);
+  console.log('request.body', request.body);
+  let equation = request.body;
+  console.log('equation', eval(equation.val));
+  bundleCalculations.push(eval(equation.val));
+  console.log(bundleCalculations);
+  let ok = 200;
+  response.send(ok);
+});
+
+app.get('/result', function(request, response) {
+  console.log('Ready to send back the result');
+  console.log('request.route.path is', request.route.path);
+  response.send(bundleCalculations);
+});
+
 
 app.listen(PORT, () => {
   //console.log ('Server is running on port', PORT)
